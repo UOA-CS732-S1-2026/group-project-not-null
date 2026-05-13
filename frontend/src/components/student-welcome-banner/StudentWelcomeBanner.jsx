bca30e9d5be96c2e2f9197c669db7c3b4114dfb0
akhil-2207 <akhilmathew2207@gmail.com>
2026-05-13 13:54:00 +1200

    Initial commit for UI cleanup student side

--- frontend/src/components/student-welcome-banner/StudentWelcomeBanner.jsx
+++ frontend/src/components/student-welcome-banner/StudentWelcomeBanner.jsx
@@ -10,7 +10,7 @@ export default function StudentWelcomeBanner() {
       </div>
       <div className="home-title-actions">
         <Link className="button button-primary" to="/tickets/new">
-          Create ticket
+          Raise Querry!
         </Link>
       </div>
     </section>
